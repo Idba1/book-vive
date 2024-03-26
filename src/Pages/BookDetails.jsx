5d7f4f9b51fd9de65b77dbcb79d28912a8f1adc9
Idba1 <moniraislamself@gmail.com>
2024-03-26 19:29:56 +0600

    book details card resposive for mobile

--- src/Pages/BookDetails.jsx
+++ src/Pages/BookDetails.jsx
@@ -20,25 +20,27 @@ const BookDetails = () => {
             setSingleBooks(singleBooks)
         }
     }, [books, bookId])
-    const { Name } = singleBooks
+    // const { Name } = singleBooks
     return (
         <div>
-            <div className="card card-side bg-base-100 shadow-xl p-16 ">
-                <figure className="w-1/3 p-10 rounded-xl"><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" className="rounded-xl" /></figure>
-                <div className="card-body w-7/12">
+            <div className="card card-normal lg:card-side bg-base-100 shadow-xl p-2 lg:p-16 ">
+                <figure className="lg:w-1/3 p-10 rounded-xl"><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" className="rounded-xl" /></figure>
+                <div className="card-body lg:w-7/12">
                     <h2 className="card-title">Book Name</h2>
                     <p>Author Name</p>
                     <hr />
                     <p>Category</p>
                     <hr />
                     <h6>Review: Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cum vel facilis nulla inventore minima aliquid omnis iure ipsa dicta voluptates laudantium repellendus tenetur, commodi mollitia cupiditate non debitis sit esse voluptatum et eos. Nulla voluptas rerum eius unde quo omnis, assumenda, modi aspernatur accusamus pariatur inventore adipisci, neque officiis.</h6>
-                    <div className="flex gap-12">
+                    <div className="lg:flex flex-col lg:gap-x-12">
                         <h1>Tag</h1>
-                        <span>TagOne</span>
-                        <span>TagTwo</span>
+                        <div className="flex justify-between gap-3">
+                            <span>TagOne</span>
+                            <span>TagTwo</span>
+                        </div>
                     </div>
                     <hr />
-                    <div>
+                    <div className="">
                         <p>Number of Pages:</p>
                         <p>Publisher:</p>
                         <p>Year of Publishing:</p>
