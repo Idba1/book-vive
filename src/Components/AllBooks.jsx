a037fb5c674c6d342fe33e98495172b958894c32
Idba1 <moniraislamself@gmail.com>
2024-03-26 16:05:54 +0600

    update book section

--- src/Components/AllBooks.jsx
+++ src/Components/AllBooks.jsx
@@ -15,11 +15,12 @@ const AllBooks = () => {
 
     return (
         <div>
-            <div className="space-y-2 text-center">
-                <h2 className="text-3xl font-bold">Books</h2>
+            <div className="space-y-2 text-center m-16">
+                <h2 className="text-3xl font-bold ">Books</h2>
+                <p>Books ignite emotions, from joyous adventure to  <br />poignant sorrow, offering solace and inspiration.  Cherish them.</p>
             </div>
 
-            <div className="grid grid-cols-3">
+            <div className="grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-2 lg:grid-cols-3">
             {
                 books.map((book) =>
                 (<Book key={book.id}
--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -5,16 +5,18 @@ const Book = ({ book }) => {
     // console.log(bookName);
     return (
         <div>
-            <div className="">
+            <div className=" rounded-xl">
 
-                <article className="lg:p-10 flex flex-col dark:bg-gray-50">
+                <article className="lg:p-10 flex flex-col ">
                     <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                        <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src={image} />
+                        <div className='bg-[#F3F3F3] py-10 px-5  rounded-xl'>
+                        <img alt="" className="object-cover w-fit mx-auto h-40 rounded-lg justify-center items-center " src={image} />
+                        </div>
                     </a>
                     <div className="flex flex-col flex-1 p-6">
                         <div className="flex justify-between">
-                            <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">{tags[0]}</a>
-                            <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600">{tags[1]}</a>
+                            <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600 bg-[#F3F3F3] rounded-2xl p-4">{tags[0]}</a>
+                            <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600 bg-[#F3F3F3] rounded-2xl p-4">{tags[1]}</a>
                         </div>
                         <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">{bookName}</h3>
                         <p>By: {author}</p>
